22f97318369b37f5fc93c4ef890447d631f5cca9
taevenp023 <taeven.peterson@umontana.edu>
2024-10-21 22:22:59 -0600

    newspaper app

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
+    "articles",
     "pages",
     "crispy_forms",
     "crispy_bootstrap5",
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Denver"
 
 USE_I18N = True
 
